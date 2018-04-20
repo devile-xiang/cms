39050e933169dd9de92f66f71f413552357e0d4c
devile-xiang <2904633401@qq.com>
2018-04-20 10:29:31 +0800

    first shangchuanwenjian

---
+++ public/admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/4/16
+ * Time: 15:14
+ */
+define('APP_PATH', __DIR__ . '/../application/');
+// 加载框架引导文件
+//定义常量
+define('BIND_MODULE','admin');
+require __DIR__ . '/../thinkphp/start.php';
